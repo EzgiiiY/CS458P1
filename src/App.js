0dc6c48fd05fc30ab68a202f0f628cdbe13f1797
ezgi <ezqi401@hotmail.com>
2021-02-15 13:43:28 +0300

    init2 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/components/App.js
+import React, { Component } from 'react';
+import { Router, Route, Switch, Link, Redirect } from 'react-router-dom'; // added
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
+import history from './history'; // added
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+const { Header, Content, Footer } = Layout;
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router history={history}>
+      <Header> HELLO </Header>
+
+          <Content >
+            <Switch>
+            
+            <Route path='/home'>
+                {//<HomePage/>
+  }
+            </Route>
+              {//<LandingPage />
+  }
+            </Switch>
+          </Content>
+          <Footer className='App-footer' style={{ textAlign: 'center'}}>
+            
+          </Footer>
+          </Router>
+
+    );
+  }
+};
 
 export default App;
